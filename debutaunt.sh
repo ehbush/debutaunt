81ff1a09804f1a88461f586a9b546773bc794f07
Anthony Bush <57749206+ehbush@users.noreply.github.com>
2021-06-02 20:36:39 -0700

    Update debutaunt.sh

--- debutaunt.sh
+++ debutaunt.sh
@@ -83,7 +83,7 @@ fi
 
 # Display Script Intro
 
-echo "${bblue} ${fmagenta}$INTRO${reset}"
+echo "\e[40;38;5;82m Hello \e[30;48;5;82m World \e[0m"$INTRO${reset}"
 
 if [[ ! -n $uOff ]]; then
     echo -e "
